7f92534d67b053f0d8d71fd3e310cef360f0fea7
Abdelrahman Elshrif <A.Adel2427@nu.edu.eg>
2025-12-18 18:18:18 +0200

    .
    Updated the stored_books class to include file handling and sorting functionality. Modified insert method to allow setting the borrowed state.

--- abdulrahman.cpp
+++ abdulrahman.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
 #include<string>
+#include<fstream>
 using namespace std;
 class stored_books{
     public:
     string book_name;
     stored_books* next;
     bool is_borrowed;
-    static int count;
+    // static int count;
     stored_books(string b,stored_books* n=NULL,bool state=false){
         book_name=b;
         next=n;
         is_borrowed=state;
-        count++;
+        // count++;
     }
     void print_books(){
         stored_books* temp=this;
@@ -27,10 +28,11 @@ class stored_books{
             cout<<endl;
             temp=temp->next;
         }
-        cout<<"count = "<<count<<endl;
+        // cout<<"count = "<<count<<endl;
     }
-    void insert(stored_books* head, string new_book){
+    void insert(stored_books* head, string new_book, bool state=false){
         stored_books* n=new stored_books(new_book);
+        n->is_borrowed=state;
         if(head==NULL){
             head=n;
             return;
@@ -45,16 +47,27 @@ class stored_books{
     }
     void bubblesort(stored_books* head){
         if(head==NULL) return;
-
+        for(stored_books* i=head;i!=NULL;i=i->next){
+            for(stored_books* j=head;j->next!=NULL;j=j->next){
+                if(j->book_name[0] > j->next->book_name[0]){
+                    swap(j->book_name,j->next->book_name);
+                    swap(j->is_borrowed,j->next->is_borrowed);
+                }
+            }
+        }
     }  
 };
-int stored_books::count = 0;
+// int stored_books::count = 0;
 int main(){
     stored_books* head=new stored_books("A");
-    head->insert(head,"B");
-    head->insert(head,"C");
+    head->insert(head,"F");                  
+    head->insert(head,"B");                  
     head->insert(head,"D");
     head->insert(head,"E");
+    head->insert(head,"C",true);
+    head->print_books();
+    head->bubblesort(head);
+    cout<<"sort: "<<endl;
     head->print_books();
     return 0;
 }
